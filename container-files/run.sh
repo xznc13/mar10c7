26be64bed852294d537dcbed57a4a7e535818cdb
Stephen Butcher <stephen.butcher@outlook.com>
2016-05-10 09:39:07 +0100

    Update run.sh

--- container-files/run.sh
+++ container-files/run.sh
@@ -13,7 +13,6 @@ VOLUME_HOME="/var/lib/mysql"
 ERROR_LOG="$VOLUME_HOME/error.log"
 MYSQLD_PID_FILE="$VOLUME_HOME/mysql.pid"
 
-/usr/sbin/sshd
 # Trap INT and TERM signals to do clean DB shutdown
 trap terminate_db SIGINT SIGTERM
 
